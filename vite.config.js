4c415360b23c0ec54db44715c9dcc9ffd033e2f8
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-10-02 11:43:16 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+      babel: {
+        plugins: []
+      }
+    })
+  ],
   server: {
     port: 3000
   },
   build: {
     outDir: 'dist',
     minify: 'esbuild',
-    sourcemap: false
+    sourcemap: false,
+    target: 'es2015'
+  },
+  define: {
+    'process.env.NODE_ENV': '"production"'
   }
 })
